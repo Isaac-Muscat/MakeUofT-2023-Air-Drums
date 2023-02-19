4cff439184d2e0bfdcb6aa05a724a19719246d83
Isaac Muscat <71135676+Isaac-Muscat@users.noreply.github.com>
2023-02-19 09:28:07 -0500

    Added Python soundplayer

---
+++ soundplayer.py
@@ -0,0 +1,44 @@
+import serial
+import subprocess
+import ffmpeg
+#from playsound import playsound
+import pygame
+
+pygame.init()
+pygame.mixer.init()
+
+snare = pygame.mixer.Sound('snare.mp3')
+hihat = pygame.mixer.Sound('hihat.mp3')
+crash = pygame.mixer.Sound('crash.mp3')
+splash = pygame.mixer.Sound('splash.mp3')
+tom = pygame.mixer.Sound('tom.mp3')
+
+arduino = serial.Serial('COM6', 115200, timeout=.1)
+
+while True:
+    data = arduino.readline()[:-2] 
+
+    if data == b'':
+        continue
+
+    data = data.decode()
+
+    if data[0] == "t":
+        snare.play()
+        print("LSnare\n")
+    
+    if data[1] == "s":
+        snare.play()
+        print("RSnare\n")
+    elif data[1] == "h":
+        hihat.play()
+        print("RHihat\n")
+    elif data[1] == "c":
+        crash.play()
+        print("RCrash\n")
+    elif data[1] == "p":
+        splash.play()
+        print("RSplash\n")
+    elif data[1] == "t":
+        tom.play()
+        print("RTom\n")
